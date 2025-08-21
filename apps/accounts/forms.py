f7ce91681de45881e449385bbe26b96d9303a239
KoRRaZon <anton.workchar@gmail.com>
2025-08-21 17:09:25 +0300

    feat(wiki): profile views

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,6 +1,22 @@
 from django import forms
 
-from apps.accounts.models import CustomUser
+from apps.accounts.models import CustomUser, Player, Master
+
+# формы для редактирования профиля
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name']
+
+class PlayerForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = ['avatar', 'nickname', 'description']
+
+class MasterForm(forms.ModelForm):
+    class Meta:
+        model = Master
+        fields = ['avatar', 'nickname', 'description']
 
 
 class UserRegisterForm(forms.ModelForm):
@@ -21,3 +37,12 @@ class UserRegisterForm(forms.ModelForm):
         if password and password_confirm and password != password_confirm:
             raise forms.ValidationError("Пароли не совпадают")
         return cleaned_data
+
+class UserLoginForm(forms.ModelForm):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password', 'password_confirm')
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from apps.accounts.views import UserRegisterView, UserLoginView, UserLogoutView
+from apps.accounts.views import UserRegisterView, UserLoginView, UserLogoutView, OwnProfileView, PublicProfileView, \
+    ProfileUpdateView
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('profile/', OwnProfileView.as_view(), name='profile'),
+    path('profile/edit', ProfileUpdateView.as_view(), name='profile_edit'),
+    path('profile_detail/<uuid:pk>/', PublicProfileView.as_view(), name='profile_detail'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
